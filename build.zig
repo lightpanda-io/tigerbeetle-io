19ae89eb3814d48c202ac9e0495fc5cadb29dfe7
Karl Seguin <karlseguin@users.noreply.github.com>
2025-08-20 14:52:38 +0800

    allow target/optimize to be configured in build (#17)

--- build.zig
+++ build.zig
@@ -1,7 +1,12 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     _ = b.addModule("tigerbeetle_io", .{
         .root_source_file = b.path("io.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 }
